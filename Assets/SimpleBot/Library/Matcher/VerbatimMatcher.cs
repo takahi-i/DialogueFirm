e8268d0a3075f7aef453c5002876d47aef24c401
takahi-i <takahi-i@cookpad.com>
2018-08-25 13:41:29 +0900

    Implement VerbatimMatcher

--- Assets/SimpleBot/Library/Matcher/VerbatimMatcher.cs
+++ Assets/SimpleBot/Library/Matcher/VerbatimMatcher.cs
@@ -1,9 +1,24 @@
-﻿namespace SimpleBot {
-  namespace Matcher {
-    abstract class VerbatimMatcher : IntentMatcher {
-	override public bool Match(string input) {
-		  return true;
-	  }
+using System.Collections.Generic;
+
+namespace SimpleBot
+{
+    namespace Matcher
+    {
+        class VerbatimMatcher : IntentMatcher
+        {
+            private HashSet<string> patterns;
+            private string name;
+
+            public VerbatimMatcher(string name, List<string> patterns)
+            {
+              this.name = name;
+              this.patterns = new HashSet<string>(patterns);
+            }
+
+            override public bool Match(string input)
+            {
+                return true;
+            }
+        }
     }
-  }
 }
