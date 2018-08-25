b479381e99ec75b961ee86a8d4f52d5dc2e4966c
takahi-i <takahi-i@cookpad.com>
2018-08-25 14:09:38 +0900

    Add test for VerbatimMatcher

---
+++ Assets/SimpleBot/Editor/VerbatimMatcherTest.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleBot.Matcher;
+
+public class VerbatimMatcherTest {
+
+    [Test]
+    public void TestMatch() {
+        VerbatimMatcher matcher = new VerbatimMatcher("foobar", new List<string>(){ "foobar" });
+        Assert.AreEqual(true, matcher.Match("this is a foobar"));
+        Assert.AreEqual(true, matcher.Match("this is a ahor"));
+    }
+
+    [Test]
+    public void TestRegexMatch() {
+        var matcher2 = new VerbatimMatcher("foobar", new List<string>(){ "fo[.]bar" });
+        Assert.AreEqual(true, matcher2.Match("this is a foobar"));
+        Assert.AreEqual(true, matcher2.Match("this is a ahor"));
+    }
+}
--- Assets/SimpleBot/Library/Matcher/VerbatimMatcher.cs
+++ Assets/SimpleBot/Library/Matcher/VerbatimMatcher.cs
@@ -1,23 +1,30 @@
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace SimpleBot
 {
     namespace Matcher
     {
-        class VerbatimMatcher : IntentMatcher
+        public class VerbatimMatcher : IntentMatcher
         {
-            private HashSet<string> patterns;
+            private HashSet<Regex> patterns;
             private string name;
 
             public VerbatimMatcher(string name, List<string> patterns)
             {
-              this.name = name;
-              this.patterns = new HashSet<string>(patterns);
+                this.name = name;
+                IEnumerable<Regex> regexPatterns = patterns.Select(x => new Regex(x));
+                this.patterns = new HashSet<Regex>(regexPatterns.ToList());
             }
 
-            override public bool Match(string input)
+            public override bool Match(string input)
             {
-                return true;
+                var matched = patterns.Where(pattern => pattern.IsMatch(input) == true);
+                if (matched.Count() >= 0) {
+                    return true;
+                }
+                return false;
             }
         }
     }
