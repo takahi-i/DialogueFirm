5f4ffd0deebdccc3287f4e0164ac120ab5f08c7b
takahi-i <takahi-i@cookpad.com>
2018-08-26 13:29:27 +0900

    Add a test case for ConfigurationBuilder

---
+++ Assets/SimpleBot/Editor/ConfigurationBuilderTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SimpleBot;
+using UnityEngine;
+
+public class ConfigurationBuilderTest {
+
+   [Test]
+    public void TestBuild() {
+        var config = new ConfigurationBuilder().AddIntent("foobar", "verbatim", new List<string>(){"foobar"}).Build();
+        Assert.AreEqual(1, config.NumberofIntentions());
+        Assert.AreEqual("foobar", config.GetIntentConfigs()[0].Name);
+        Assert.AreEqual("verbatim", config.GetIntentConfigs()[0].MatcherType());
+        Assert.AreEqual("foobar", config.GetIntentConfigs()[0].Patterns()[0]);
+    }
+}
--- Assets/SimpleBot/Library/Config/ConfigurationBuilder.cs
+++ Assets/SimpleBot/Library/Config/ConfigurationBuilder.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConfigurationBuilder : MonoBehaviour {
+namespace SimpleBot {
+    public class ConfigurationBuilder {
+		private List<IntentConfig> IntentConfigs;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        public ConfigurationBuilder()
+        {
+			IntentConfigs = new List<IntentConfig>();
+        }
+
+		public ConfigurationBuilder AddIntent(string name, string type, List<string> patterns) {
+			this.IntentConfigs.Add(new IntentConfig(name, type, patterns));
+			return this;
+		}
+
+		public Configuration Build() {
+			return new Configuration(this.IntentConfigs);
+		}
+    }
 }
