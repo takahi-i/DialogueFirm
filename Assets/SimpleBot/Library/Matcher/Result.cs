ad38dbaba1dea5e070ec99cada41c3dd9dfe1a16
takahi-i <takahi-i@cookpad.com>
2018-08-26 23:16:56 +0900

    Basic implementation of Template class

---
+++ Assets/SimpleBot/Library/Matcher/Result.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleBot {
+    namespace Matcher {
+		public class Result {
+			public string input;
+			public bool success;
+			public Dictionary<string, string> features;
+
+			public Result(string input,  bool success ,Dictionary<string, string> features) {
+				this.success = success;
+				this.input = input;
+				this.features = features;
+			}
+		}	
+	}
+}
---
+++ Assets/SimpleBot/Library/Matcher/Template.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using SimpleBot.Matcher;
+
+namespace SimpleBot {
+	namespace Matcher {
+		public class Template  {
+			private List<string> slotNames;
+		    private Regex pattern;
+
+			public Template(Regex pattern, List<string> slots)  {
+				this.pattern = pattern;
+				this.slotNames = slots;
+			}
+
+			public Result Match(string input) {
+				var match = this.pattern.Match(input);
+				if (match.Success) {
+					return new Result(input, true, new Dictionary<string, string>());
+				}
+				return new Result(input, false, new Dictionary<string, string>());
+			}
+		}
+	}
+}
