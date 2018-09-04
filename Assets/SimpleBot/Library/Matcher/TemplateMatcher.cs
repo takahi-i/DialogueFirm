2a4b9dda6a94d799f526e0b77b12b0e50eaac2e2
takahi-i <takahi-i@cookpad.com>
2018-09-04 09:34:52 +0900

    Implement TeplateMatcher#GenerateTemplate

--- Assets/SimpleBot/Library/Matcher/TemplateMatcher.cs
+++ Assets/SimpleBot/Library/Matcher/TemplateMatcher.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+
 
 namespace SimpleBot
 {
@@ -16,17 +18,39 @@ namespace SimpleBot
             // slots: {"ingredient1", "ingredient"}
             // templates: { "ingredinet": ["potato", "tomato"]}
             // see https://github.com/voice-assistant/satori-flow/blob/master/src/matcher/templateIntentMatcher.js#L58
-            public Template generateTemplate(string pattern, IDictionary<string, string> slots, TypeConfig typeconfig) {
-                var startPosition = 0;
-                var endPosition = 0;
+            public Template GenerateTemplate(string pattern, IDictionary<string, string> slots, TypeConfig typeconfig)
+            {
+                int startPosition = 0;
+                int endPosition = 0;
                 var elements = new List<string>();
                 var slotNames = new List<string>();
                 var inBrace = false;
 
-                while(endPosition < pattern.Length) {
-                    
+                while (endPosition < pattern.Length)
+                {
+                    if (!inBrace)
+                    {
+                        endPosition = pattern.IndexOf("${", startPosition, StringComparison.Ordinal);
+                        if (endPosition == -1)
+                        { // no brace
+                            elements.Add(pattern.Substring(startPosition, pattern.Length));
+                            break;
+                        }
+                        elements.Add(pattern.Substring(startPosition, endPosition));
+                        inBrace = true;
+                    }
+                    else // found slot
+                    {
+                        endPosition = pattern.IndexOf("}", startPosition, StringComparison.Ordinal);
+                        var slotName = pattern.Substring(startPosition + 1, endPosition);
+                        string typeName = slots[slotName];
+                        var typeElements = typeconfig.Get(typeName);
+                        elements.Add(string.Join("|", typeElements.ToArray()));
+                        slotNames.Add(slotName);
+                        inBrace = false;
+                    }
                 }
-                return null;
+                return new Template(string.Join("", elements.ToArray()), slotNames);
             }
 
             public override bool Match(string input)
