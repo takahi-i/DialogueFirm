0e33d464b27013bd813e2255ad3edf11e9c4b58a
takahi-i <takahi-i@cookpad.com>
2018-09-09 16:18:42 +0900

    Enhance test case of template matcher

--- Assets/SimpleBot/Editor/TemplateMatcherTest.cs
+++ Assets/SimpleBot/Editor/TemplateMatcherTest.cs
@@ -18,4 +18,20 @@ public class NewTestScript {
         Template template = TemplateMatcher.GenerateTemplate(pattern, slots, typeconfig);
         Assert.AreEqual("a (?<ingredient1>potato|tomato)", template.PatternStr);
     }
+
+    [Test]
+    public void TestGenerateTemplateWithMatch()
+    {
+        string pattern = "a ${ingredient1}";
+        Dictionary<string, string> slots = new Dictionary<string, string>(){
+            {"ingredient1", "ingredient"}
+        };
+        TypeConfig typeconfig = new TypeConfig();
+        typeconfig.Add("ingredient", new List<string>() { "potato", "tomato" });
+        Template template = TemplateMatcher.GenerateTemplate(pattern, slots, typeconfig);
+        Assert.AreEqual("a (?<ingredient1>potato|tomato)", template.PatternStr);
+        Result result = template.Match("this is a potato or not.");
+        Assert.AreEqual(true, result.Success);
+        Assert.AreEqual("potato", result.Match.Groups["ingredient1"].Value);
+    }
 }
