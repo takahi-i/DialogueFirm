594bd8644c590e58ecc70461b48dea4d179ddd8a
takahi-i <takahi-i@cookpad.com>
2018-09-14 09:36:29 +0900

    Add SimpleResponderClass

---
+++ Assets/SimpleBot/Library/Responder/SimpleResponder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using SimpleBot;
+
+public class SimpleResponder {
+    private List<string> responds;
+    private Random cRandom;
+    private string targetIntent;
+
+    public SimpleResponder(string targetIntent, List<string> responds)
+    {
+        this.targetIntent = targetIntent;
+        this.responds = responds;
+        this.cRandom = new System.Random();
+    }
+
+    public string Respond(Intent intent) {
+        if (this.responds.Count == 0) {
+            throw new InvalidOperationException("No responds are deployed in the responder for intent " + targetIntent);
+        }
+        return this.responds[this.cRandom.Next(this.responds.Count-1)];
+    }
+}
