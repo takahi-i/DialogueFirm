c5c98646f01b67e91530fe1c16ce10151df46821
takahi-i <takahi-i@cookpad.com>
2018-09-15 12:23:33 +0900

    Add SimpleRespondTest

---
+++ Assets/SimpleBot/Editor/SimpleResponderTest.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using SimpleBot.Responder;
+public class SimpleResponderTest {
+
+    [Test]
+    public void RespondToIntent() {
+        SimpleResponder responder = new SimpleResponder("ingredient", new List<string>() { "yes that is very tasty."});
+        Assert.AreEqual("yes that is very tasty.", responder.Respond(new SimpleBot.Intent("ingredient", true, new Dictionary<string, string>())));
+    }
+}
