8c083c9b21c1ac118ab0c4e3948bd8a953d7d173
takahi-i <takahi-i@cookpad.com>
2018-09-15 16:23:12 +0900

    Implement BotEngine#generateResponderMap

--- Assets/SimpleBot/Library/BotEngine.cs
+++ Assets/SimpleBot/Library/BotEngine.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using SimpleBot.Responder;
 
 namespace SimpleBot
@@ -11,6 +12,18 @@ namespace SimpleBot
         public BotEngine(Configuration config)
         {
             this.identifier = new IntentIdentifier(config);
+            this.responders = this.generateResponderMap(config);
+        }
+
+        private IDictionary<string, ReplyResponder> generateResponderMap(Configuration config)
+        {
+            List<ResponderConfig> responderConfigs = config.ResponderConfigs;
+            IDictionary<string, ReplyResponder> responderMap = new Dictionary<string, ReplyResponder>();
+            foreach (var responderConfig in responderConfigs)
+            {
+                responderMap.Add(responderConfig.Target, new SimpleResponder(responderConfig.Target, responderConfig.Responds)); //TODO: support various responders
+            }
+            return responderMap;
         }
 
         public Intent IdenfityIntent(string input)
