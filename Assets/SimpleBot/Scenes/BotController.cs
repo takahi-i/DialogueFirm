ff4c871166b1dbf693bde2156f97a9046a531809
takahi-i <takahi-i@cookpad.com>
2018-09-22 12:03:07 +0900

    Add handelr to submit input text

---
+++ Assets/SimpleBot/Scenes/BotController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BotController : MonoBehaviour {
+
+
+    string str;
+    public InputField inputField;
+    public Text text;
+
+    public void SaveText()
+    {
+        str = inputField.text;
+        text.text = str;
+        inputField.text = "";
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+}
