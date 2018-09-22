0b50ee7c36d924654cccf741c8301e527f144543
takahi-i <takahi-i@cookpad.com>
2018-09-22 15:01:17 +0900

    Add process to load SampleBotConfig

--- Assets/SimpleBot/Scenes/BotController.cs
+++ Assets/SimpleBot/Scenes/BotController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using SimpleBot;
+using UnityEngine;
 using UnityEngine.UI;
 
 
@@ -8,6 +10,7 @@ public class BotController : MonoBehaviour {
     string str;
     public InputField inputField;
     public Text text;
+    private BotEngine bot;
 
     public void SaveText()
     {
@@ -18,7 +21,16 @@ public class BotController : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
+        this.loadConfig();
+    }
+
+    void loadConfig()
+    {
+        string settingFilePath = Application.dataPath + "/SimpleBot/Scenes/simple-bot-conf.json";
+        string setting = File.ReadAllText(settingFilePath);
+        Debug.Log(setting);
+        ConfigurationLoader configurationLoader = new ConfigurationLoader();
+        Configuration config = configurationLoader.loadFromString(setting);
+        this.bot = new BotEngine(config);
+    }
 }
