f3a773f08ea29f31f344a2c7ca1255473894f79f
takahi-i <takahi-i@cookpad.com>
2018-09-26 08:53:49 +0900

    Implement State class

---
+++ Assets/SimpleBot/Library/State.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class State {
+    void SetInt(string key, int value) {
+        PlayerPrefs.SetInt(key, value);
+    }
+
+    void SetString(string key, string value) {
+        PlayerPrefs.SetString(key, value);
+    }
+
+    int GetInt(string key) {
+        if (PlayerPrefs.HasKey(key)) {
+            return PlayerPrefs.GetInt(key);
+        }
+        throw new InvalidOperationException("no such key as " + key);
+    }
+
+    string GetString(string key)
+    {
+        if (PlayerPrefs.HasKey(key))
+        {
+            return PlayerPrefs.GetString(key);
+        }
+        throw new InvalidOperationException("no such key as " + key);
+    }
+}
