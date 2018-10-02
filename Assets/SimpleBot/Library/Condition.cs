e47e0f7f46cc6a079c760c57e9e0562d90cf18f1
takahi-i <takahi-i@cookpad.com>
2018-10-02 09:58:31 +0900

    Add Condition.cs

---
+++ Assets/SimpleBot/Library/Condition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace SimpleBot
+{
+    class Condition
+    {
+        static public Func<float, float> Load(ConditionConfig config)
+        {
+            return null;
+        }
+    }
+}
