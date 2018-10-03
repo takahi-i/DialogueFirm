b8510a8c30ba249ef8834ca4d4c0c20faf888af3
takahi-i <takahi-i@cookpad.com>
2018-10-03 09:08:14 +0900

    Implement Condition class

--- Assets/SimpleBot/Library/Condition.cs
+++ Assets/SimpleBot/Library/Condition.cs
@@ -2,14 +2,106 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 namespace SimpleBot
 {
     class Condition
     {
-        static public Func<float, float> Load(ConditionConfig config)
+        static public Func<State, bool> Load(ConditionConfig config)
         {
-            return null;
+            foreach (var child_condition in config.ChildConfigs) {
+                return term(child_condition.CondtionType, child_condition);
+            }
+            throw new ArgumentException("No conditon is defined ...");
+        }
+
+        private static Func<State, bool> term(string condtion_type, ConditionConfig factor_config)
+        {
+            if (condtion_type == "must")
+            {
+                return must_method(factor_config);
+            }
+            else if (condtion_type == "should")
+            {
+                return should_method(factor_config);
+            }
+            else
+            {
+                throw new ArgumentException("Condtion type, " + condtion_type + " is not supported in the top level");
+            }
+        }
+
+        private static Func<State, bool> must_method(ConditionConfig factor_config)
+        {
+            var factor_methods = generate_factor_methods(factor_config);
+            return (State state) => {
+                foreach (var factor in factor_methods)
+                {
+                    if (!factor(state))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            };
+        }
+
+        private static Func<State, bool> should_method(ConditionConfig factor_config)
+        {
+            var factor_methods = generate_factor_methods(factor_config);
+            return (State state) => {
+                foreach (var factor in factor_methods)
+                {
+                    if (factor(state))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            };
+        }
+
+        private static List<Func<State, bool>> generate_factor_methods(ConditionConfig factor_config)
+        {
+            var factor_methods = new List<Func<State, bool>>();
+            foreach (var config in factor_config.ChildConfigs)
+            {
+                Func<State, bool> factor_method = generate_factor_method(config);
+                factor_methods.Add(factor_method);
+            }
+            return factor_methods;
+        }
+
+        private static Func<State, bool> generate_factor_method(ConditionConfig config)
+        {
+            var factor_name = config.CondtionType;
+            if (factor_name == "term")
+            {
+                return term_method(config);
+            }
+            else if (factor_name == "range")
+            {
+                return ranges_method(config);
+            } else {
+                throw new ArgumentException(factor_name + " is not supported in factor method.");
+            }
+        }
+
+        private static Func<State, bool> term_method(ConditionConfig config)
+        {
+            var targetField = (string)config.Arguments[0].First;
+            var targetValue = (string)config.Arguments[0].Second;
+            return (State state) => {
+                if (state.GetString(targetField) == targetValue)
+                {
+                    return true;
+                }
+                return false;
+            };
+        }
+
+        private static Func<State, bool> ranges_method(ConditionConfig config)
+        {
+            throw new NotImplementedException();
         }
     }
 }
