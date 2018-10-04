2ef9cdda95d857127cf7331375447341316dc03a
takahi-i <takahi-i@cookpad.com>
2018-10-04 09:35:49 +0900

    Add unmatched test case

--- Assets/SimpleBot/Editor/ConditionTest.cs
+++ Assets/SimpleBot/Editor/ConditionTest.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using SimpleBot;
 using System;
+using UnityEngine;
 
 public class ConditionTest
 {
@@ -17,6 +18,18 @@ public class ConditionTest
         Func<State, bool> condition = Condition.Load(config.ResponderConfigs[0].Conditions[0]);
         this.state.SetString("status", "happy");
         Assert.AreEqual(true, condition(this.state));
+    }
+
+    [Test]
+    public void TestUnmatchedTermConditon()
+    {
+        var config = new ConfigurationBuilder().AddResponds("foobar", new List<string>() { "baz" },
+                                                            new List<ConditionConfig>() { new ConditionConfig("must", new List<ConditionConfig>() { new ConditionConfig("term", "feel", new List<Pair>() { new Pair("happy", "dummy") }) }) }).Build();
+        Assert.AreEqual(1, config.ResponderConfigs[0].Conditions.Count);
+
+        Func<State, bool> condition = Condition.Load(config.ResponderConfigs[0].Conditions[0]);
+        this.state.SetString("feel", "sad");
+        Assert.AreEqual(false, condition(this.state));
     }
 
     [TearDown]
