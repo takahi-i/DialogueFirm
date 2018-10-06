5bbbf2790d3909b61a3d67a25b91bd328c73174b
takahi-i <takahi-i@cookpad.com>
2018-10-06 15:19:43 +0900

    Add first test for ConditionConfigBuilder

---
+++ Assets/SimpleBot/Editor/ConditionConfigTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using SimpleBot;
+
+public class ConditionConfigTest
+{
+
+    [Test]
+    public void TestSimpleLoad()
+    {
+        var config = new ConditionConfigBuilder().
+                                                 AddType("must")
+                                                 .AddChild(new ConditionConfigBuilder()
+                                                          .AddType("term")
+                                                          .AddTargetField("status")
+                                                          .AddArgument(new Pair("happy", "dummy")).Build())
+                                                 .Build();
+        Assert.AreEqual("must", config.CondtionType);
+    }
+}
