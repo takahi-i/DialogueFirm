4e1f6448723ce4d4ad2b919a8ab602e9622a1c43
takahi-i <takahi-i@cookpad.com>
2018-10-07 22:48:15 +0900

    First implementation of Effect

---
+++ Assets/SimpleBot/Editor/EffectTest.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class EffectTest {
+
+
+}
---
+++ Assets/SimpleBot/Library/Effect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SimpleBot
+{
+    public class Effect
+    {
+        private Func<State, bool> apply;
+        private string targetField;
+
+        public Effect(EffectConfig config)
+        {
+            targetField = config.TargetFIeld;
+            apply = this.load(config);
+        }
+
+        private Func<State, bool> load(EffectConfig config)
+        {
+            if (config.EffectType == "incr")
+            {
+                return (State state) => {
+                    if (state.HasKey(targetField))
+                    {
+                        int result = state.GetInt(targetField);
+                        state.SetInt(targetField, ++result); 
+                    }
+                    return true;
+                };
+            } else {
+                throw new ArgumentException(config.EffectType + " is not supported");
+            }
+        }
+    }
+}
+
