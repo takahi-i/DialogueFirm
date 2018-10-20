2e034683229717d47b6b08fa54775cc971fdee75
takahi-i <takahi-i@cookpad.com>
2018-10-21 00:30:23 +0900

    Add release manager demo

---
+++ Assets/SimpleBot/Scripts/ReleaseManager/ManagerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using System.IO;
+using SimpleBot;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// Bot controller.
+/// </summary>
+public class ManagerController : MonoBehaviour
+{
+    string str;
+    public InputField inputField;
+    public Text text;
+    private BotEngine bot;
+
+    public void SaveText()
+    {
+        if (this.bot == null)
+        {
+            Debug.Log("loading config");
+            this.loadConfig();
+        }
+        else
+        {
+            Debug.Log("exist config");
+        }
+
+        str = inputField.text;
+        Debug.Log("input: " + str);
+        var reply = this.bot.replySentence(str);
+        text.text = reply;
+        inputField.text = "";
+    }
+
+    private void loadConfig()
+    {
+        string settingFilePath = Application.dataPath + "/SimpleBot/Settings/ReleaseManager/manager-conf.json";
+        string setting = File.ReadAllText(settingFilePath);
+        Debug.Log(setting);
+        ConfigurationLoader configurationLoader = new ConfigurationLoader();
+        Configuration config = configurationLoader.loadFromString(setting);
+        this.bot = new BotEngine(config);
+        Debug.Log("Finished loading bot");
+    }
+}
