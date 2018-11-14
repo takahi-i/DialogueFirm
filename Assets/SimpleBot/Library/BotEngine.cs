1757b0546fccad917a9fc0e92d68a374edb8cba9
takahi-i <takahi-i@cookpad.com>
2018-11-14 09:32:36 +0900

    Support string parameter in the constructor of BotEngine

--- Assets/SimpleBot/Library/BotEngine.cs
+++ Assets/SimpleBot/Library/BotEngine.cs
@@ -21,10 +21,21 @@ namespace SimpleBot
         /// <param name="config">Configuration object loaded from the configuration file</param>
         public BotEngine(Configuration config)
         {
+            setResources(config);
+        }
+
+        public BotEngine(string configurationString) {
+            ConfigurationLoader configurationLoader = new ConfigurationLoader();
+            Configuration config = configurationLoader.loadFromString(configurationString);
+            setResources(config);
+        }
+
+        private void setResources(Configuration config) {
             this.identifier = new IntentIdentifier(config);
             this.responders = this.generateResponderMap(config);
             this.state = new State();
         }
+
 
         private IDictionary<string, List<ReplyResponder>> generateResponderMap(Configuration config)
         {
