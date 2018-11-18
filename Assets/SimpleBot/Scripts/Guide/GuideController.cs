a3cd1a65d4e5ba7b6c6b1abacd6f873e6bcfc346
takahi-i <takahi-i@cookpad.com>
2018-11-18 16:15:10 +0900

    Extract ChangeImage

--- Assets/SimpleBot/Scripts/Guide/GuideController.cs
+++ Assets/SimpleBot/Scripts/Guide/GuideController.cs
@@ -8,9 +8,6 @@ using UnityEngine.UI;
 /// Bot controller.
 /// </summary>
 public class GuideController : MonoBehaviour {
-
-
-    string str;
     public InputField inputField;
     public Text text;
     private BotEngine bot;
@@ -19,30 +16,26 @@ public class GuideController : MonoBehaviour {
     public Sprite defaultSprite;
     public Image guideImage;
 
-
     public void Start()
     {
         confusedSprite = Resources.Load<Sprite>("SimpleBot/Guide/bulter-confused") as Sprite;
         happySprite = Resources.Load<Sprite>("SimpleBot/Guide/bulter-smile") as Sprite;
         defaultSprite = Resources.Load<Sprite>("SimpleBot/Guide/butler-default") as Sprite;
         guideImage.sprite = defaultSprite;
+        this.LoadConfig();
     }
 
     public void SaveText()
     {
-        if (this.bot == null) {
-            Debug.Log("loading config");
-            this.LoadConfig();
-        } else {
-            Debug.Log("exist config");
-        }
-        str = inputField.text;
-        Debug.Log("input: " + str);
-        var reply = this.bot.ReplySentence(str);
+        var reply = this.bot.ReplySentence(inputField.text);
+		this.ChangeImage(bot.State.GetInt("fail-count"));
+        text.text = reply;
+        inputField.text = "";
+    }
 
-        int failCount = bot.State.GetInt("fail-count");
-        Debug.Log("failCount" + failCount.ToString());
-        if (failCount == 0)
+	void ChangeImage(int failCount)
+	{
+		if (failCount == 0)
         {
             guideImage.sprite = happySprite;
         }
@@ -54,25 +47,20 @@ public class GuideController : MonoBehaviour {
         {
             guideImage.sprite = confusedSprite;
         }
-
-        text.text = reply;
-        inputField.text = "";
-    }
+	}
 
     void LoadConfig()
     {
-        
         string settingFilePath = this.GetStreamingAssetsPath("SimpleBot/Guide/guide-conf.json");
         string settingString = File.ReadAllText(settingFilePath);
         this.bot = new BotEngine(settingString);
-        Debug.Log("Finished loading bot");
     }
 
     string GetStreamingAssetsPath(string suffix)
     {
         string path;
 #if UNITY_EDITOR
-        path = Application.dataPath + "/StreamingAssets/";
+     path = Application.dataPath + "/StreamingAssets/";
 #elif UNITY_ANDROID
      path = "jar:file://"+ Application.dataPath + "!/assets/";
 #elif UNITY_IOS
@@ -82,7 +70,6 @@ public class GuideController : MonoBehaviour {
      path = Application.dataPath + "/StreamingAssets/";
 #endif
         path = path + suffix;
-        Debug.Log("Path of setting file: " + path);
         return path;
     }
 }
